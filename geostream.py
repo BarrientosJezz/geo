b7b0c05de4b90e8a101686228d16d154e89b59a9
BarrientosJezz <161073229+BarrientosJezz@users.noreply.github.com>
2025-06-11 15:47:50 -0400

    Update geostream.py

--- geostream.py
+++ geostream.py
@@ -44,7 +44,7 @@ def calcular_distancia(coord1, coord2):
 
 def main():
     st.title('🗺️ Buscador de Rutas Cercanas')
-    
+    st.title('v 11/04/2025')
     # Usar ruta relativa para el archivo Excel
     ruta_archivo = 'GEOS.xlsx'
     
